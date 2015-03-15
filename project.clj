426be2746f6959bb727fc64dbab0100d81e79e40
Daniel E. Renfer <duck@kronkltd.net>
2015-03-15 09:41:14 -0400

    bump versions

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "http://github.com/duck1123/ciste"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :min-lein-version "2.0.0"
-  :dependencies [[ciste/ciste-core "0.4.0-SNAPSHOT"]
-                 [clj-tigase "0.1.0"]]
+  :dependencies [[ciste "0.6.0-SNAPSHOT"]
+                 [clj-tigase "0.2.0-SNAPSHOT"]]
   :profiles {:dev
              {:dependencies
               [[log4j "1.2.17"]
-               [midje "1.5-alpha3"]]}}
-  :plugins [[codox "0.6.4"]
-            [lein-midje "2.0.3"]]
+               [midje "1.7.0-SNAPSHOT"]
+               [org.slf4j/slf4j-log4j12 "1.7.10"]]}}
+  :plugins [[codox "0.8.10"]
+            [lein-midje "3.1.3"]]
   :autodoc {:name "Ciste Service Tigase"
-            :copyright "2012 KRONK Ltd."})
+            :copyright "2015 KRONK Ltd."})
